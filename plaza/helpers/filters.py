5bd19b34118c9fb7fcb031fb59d48567ab2ac813
Phillip Malboeuf <phil@boeuf.coffee>
2017-11-15 10:46:37 -0500

    Emailize

--- plaza/helpers/filters.py
+++ plaza/helpers/filters.py
@@ -5,3 +5,9 @@ from plaza import app
 @app.template_filter('date')
 def date_filter(date):
     return date.strftime('%b %d, %Y') 
+
+
+@app.template_filter('emailize')
+def emailize_filter(text):
+	words = [word if '@' not in word else '<a href="mailto:{0}">{0}</a>'.format(word) for word in text.split(" ") ]
+	return " ".join(words)
